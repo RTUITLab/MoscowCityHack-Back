4cc1eb914d5e5af9d8b653ebaf87266cb19db0d2
gigagibi <gii01@mail.ru>
2022-06-10 20:21:50 +0300

    init

---
+++ src/main/java/com/example/moscowcityhackback/MoscowCityHackBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.moscowcityhackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoscowCityHackBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoscowCityHackBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moscowcityhackback/MoscowCityHackBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moscowcityhackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoscowCityHackBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
