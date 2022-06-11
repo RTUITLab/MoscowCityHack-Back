c0ce9d07af454b4135a4fd1f17407f7b21dd1439
redbird <redbird8941@gmail.com>
2022-06-11 13:57:12 +0300

    add test init data

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,13 @@
+insert into roles
+    (name)
+values ('moderator'),
+       ('volunteer'),
+       ('company');
+
+insert into users
+    (login, password, role_id)
+values
+       -- password == login
+       ('mod', '$2a$12$L5.NuVH/8M53G5r6XwydaOUkW2i7TvREZjQ/6rHxLA5wawaR7DDt6', 1),
+       ('user', '$2a$12$qQUkbLa32AktphPOFvZrzeOGnQEKXzxh1v1vukbSIhA4wKbFso3Ly', 2),
+       ('com', '$2a$12$YabWTnC.ZPRPYolMlROaCunTA1PzfCg7U/a7B0sQfqnwiFHjNoGXa', 3);
