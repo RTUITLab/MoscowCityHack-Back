46f0d0e0b4673889da4efc411d298f0a7dd7a7a4
redbird <redbird8941@gmail.com>
2022-06-11 19:01:49 +0300

    add user progress models

---
+++ src/main/java/com/example/moscowcityhackback/entity/advancement/Advancement.java
@@ -0,0 +1,32 @@
+package com.example.moscowcityhackback.entity.advancement;
+
+import com.example.moscowcityhackback.entity.AbstractEntity;
+import com.example.moscowcityhackback.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "company")
+public class Advancement extends AbstractEntity {
+
+    @ManyToOne
+    private User user;
+    private Integer level;
+    private Integer points;
+
+    @OneToMany(mappedBy = "advancement")
+    private List<PointsHistory> pointsHistory = new java.util.ArrayList<>();
+
+    @ManyToMany
+    private List<Achievement> achievements;
+    private Integer exp;
+
+
+}
---
+++ src/main/java/com/example/moscowcityhackback/entity/advancement/PointsHistory.java
@@ -0,0 +1,27 @@
+package com.example.moscowcityhackback.entity.advancement;
+
+import com.example.moscowcityhackback.entity.AbstractEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "points_history")
+public class PointsHistory extends AbstractEntity {
+    @ManyToOne
+    @JoinColumn(name = "advancement_id")
+    private Advancement advancement;
+
+    private Integer points;
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private Instant createdDate;
+}
