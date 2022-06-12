a63f7ec1d6073c9957cac962f6a5ba24909c0f93
gigagibi <gii01@mail.ru>
2022-06-12 18:56:05 +0300

    directory moves

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -12,7 +12,7 @@ values
     ('user', '$2a$12$qQUkbLa32AktphPOFvZrzeOGnQEKXzxh1v1vukbSIhA4wKbFso3Ly', 2),
     ('com', '$2a$12$YabWTnC.ZPRPYolMlROaCunTA1PzfCg7U/a7B0sQfqnwiFHjNoGXa', 3);
 
-insert into event
+insert into events
     (title, online, photo_url, address, region, date_start, date_end, published, current_amount, max_amount, email, owner_id)
 values ('Волонтеры охотники за приведениями', false, 'https://images.unsplash.com/photo-1634840647366-e755dc76882d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1620&q=80',
         'адрес проспект вернадского', 'center', 1654666200, 1654678800, true, 1, 10, 'pomogite@help.me', 3);
